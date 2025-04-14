97735736aaccc8b6d30ab04f985df647a10ef409
tomy-cyber <tomyericson1987@gmail.com>
2025-04-14 02:59:55 -0700

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -91,7 +91,7 @@ import Cors from "cors"
 // Configure CORS options
 export const cors = Cors({
   origin: ["https://front-end-f.vercel.app", "https://admin-panel-kappa-lyart.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
   maxAge: 86400, // 24 hours in seconds
